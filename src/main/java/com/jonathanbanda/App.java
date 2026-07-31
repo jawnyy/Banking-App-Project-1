73edaf19fcdf45e171c1fd280ebeb3908a5e30cf
Jonathan Banda <bandajonathan07@gmail.com>
2026-07-31 10:44:45 -0500

    Initial commit.

---
+++ src/main/java/com/jonathanbanda/App.java
@@ -0,0 +1,18 @@
+package com.jonathanbanda;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+
+        Scanner sc = new Scanner(System.in);
+        String test = sc.nextLine();
+        System.out.println(test);
+    }
+}
